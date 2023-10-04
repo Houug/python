e00cf9a1c937c45226b19ab83212351ac14545e5
Houug <75481666+Houug@users.noreply.github.com>
2023-10-04 19:50:48 +0300

    Занятие 1

---
+++ main.py
@@ -0,0 +1,68 @@
+print("Привет Артем!")
+print("Привет Всем!")
+print("Сегодня будет домашка!!")
+print("Завтра в школу")
+
+name = "Роман"
+print(name)
+print(name)
+name = "Артем"
+print(name)
+print(name)
+print(name)
+
+privet = "Привет " + name
+print(privet)
+
+name2 = name + name
+print(name2)
+
+#my_name = input("Введи свое имя: ")
+#print("Привет", my_name, my_name)
+
+# Имя, возраст, пол, город, цвет
+# Твои данные: ....
+# Мое имя: <Артем> !!!
+# "Мое имя: " + name + " !!!"
+# \n - перенос строки
+#print(f"Мое имя: {my_name} !!!\n\n\
+#Другая")
+
+# int - 10
+# float - 9.61293
+# str - "Artem 10"
+# bool True либо False
+# + - * / ** %
+
+num1 = int(input("Введите число 1: "))
+num2 = int(input("Введите число 2: "))
+print("Сложение: ", num1 + num2)
+print("Умножение: ", num1 * num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
